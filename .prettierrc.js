a752e11eea305c14b41fef00285408f52c88734d
Edgar Zea <cesszea@icloud.com>
2023-11-14 17:22:01 -0600

    feat(ci): adding stylelint, showing files'

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
   arrowParens: 'avoid',
+  bracketSameLine: false,
   bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'strict',
   jsxSingleQuote: true,
   printWidth: 100,
   semi: true,
