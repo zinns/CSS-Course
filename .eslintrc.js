01c8ed1d621f98e0dbe8f9c7510a3859ca1388ad
Edgar Zea <cesszea@icloud.com>
2024-09-06 16:11:50 -0600

    fix(linter): remove warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     node: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+  ignores: ['node_modules/*, .prettierignore'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
