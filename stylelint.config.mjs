15fd1b2a95f1e16d9e7c565493817b41a34e982d
Edgar Zea <cesszea@icloud.com>
2025-10-07 20:57:22 -0600

    fix(yarn): dependencies

---
+++ stylelint.config.mjs
@@ -0,0 +1,4 @@
+// stylelint.config.mjs  (ESM is fine for v16+)
+/** @type {import('stylelint').Config} */
+
+export default { extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'] };
